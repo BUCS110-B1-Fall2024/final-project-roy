6ca421be7ef85ad093c914bd5c16a14b41dc5758
Royy99 <ryun1@binghamton.edu>
2024-12-04 16:36:42 -0500

    Update models.py

--- template_final_project-master/src/models.py
+++ template_final_project-master/src/models.py
@@ -1,1 +1,18 @@
+class GameModel:
+    def __init__(self):
+        self.score = 0
+        self.timer = 30
+        self.high_score = self.load_high_score()
+
+    @staticmethod
+    def load_high_score():
+        try:
+            with open("etc/high_scores.txt", "r") as file:
+                return int(file.read())
+        except FileNotFoundError:
+            return 0
+
+    def save_high_score(self):
+        with open("etc/high_scores.txt", "w") as file:
+            file.write(str(self.high_score))
 
